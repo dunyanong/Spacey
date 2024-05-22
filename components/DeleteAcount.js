25dd457ab238507347417db2358e83ab2784950a
Dun Yan <ong.dy2004@gmail.com>
2024-05-22 21:00:25 +0800

    first commit

---
+++ components/DeleteAcount.js
@@ -0,0 +1,107 @@
+import { useRouter } from "next/router";
+import { auth } from "../utils/firebase";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+import { useState } from "react";
+
+const Danger = () => {
+  const router = useRouter();
+  const [showConfirmSignout, setShowConfirmSignout] = useState(false);
+  const [showConfirmDelete, setShowConfirmDelete] = useState(false);
+
+  const handleSignoutAccount = async () => {
+    try {
+      await auth.signOut();
+      toast.success("Signed out 🤘", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      });
+      router.push("/");
+    } catch (error) {
+      toast.error(error.message, {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 5000,
+      });
+    }
+  };  
+
+  const handleTerminateAccount = async () => {
+    try {
+      await auth.currentUser.delete();
+      toast.success("Account terminated 🚀", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500,
+      });
+      router.push("/");
+    } catch (error) {
+      toast.error(error.message, {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 5000,
+      });
+    }
+  };
+
+  return (
+    <div className="pt-0">
+      {showConfirmSignout && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          <div className="bg-white rounded-lg p-4 z-50 mx-4 md:mx-0">
+            <p className="mb-4">Are you sure you want to signout</p>
+            <div className="flex justify-end">
+              <button className="text-white bg-red-500 py-2 px-4 rounded-lg mr-2" onClick={handleSignoutAccount}>
+                Signout
+              </button>
+              <button className="text-gray-600 bg-gray-300 py-2 px-4 rounded-lg" onClick={() => setShowConfirmSignout(false)}>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>        
+      )}
+      {showConfirmDelete && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          <div className="bg-white rounded-lg p-4 z-50 mx-4 md:mx-0">
+            <p className="mb-4">Are you sure you want to terminate your account? This action is irreversible.</p>
+            <div className="flex justify-end">
+              <button className="text-white bg-red-500 py-2 px-4 rounded-lg mr-2" onClick={handleTerminateAccount}>
+                Yes, terminate my account
+              </button>
+              <button className="text-gray-600 bg-gray-300 py-2 px-4 rounded-lg" onClick={() => setShowConfirmDelete(false)}>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}      
+
+      <div className="rounded-lg border bg-card text-card-foreground shadow-sm my-4">
+        <div className="flex flex-col space-y-1.5 p-6">
+          <h3 className="text-lg font-semibold leading-none tracking-tight">Sign Out Account</h3>
+          <p className="text-sm text-muted-foreground">Disconnect from your account and return to the login screen.</p>
+        </div>
+        <div className="flex items-center p-6 pt-0">
+          <button className="inline-flex text-white items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-black text-primary-foreground h-10 py-2 px-4" onClick={() => setShowConfirmSignout(true)}>
+            Sign out
+          </button>
+        </div>
+      </div>
+
+      <div className="rounded-lg border bg-card text-card-foreground shadow-sm my-4">
+        <div className="flex flex-col space-y-1.5 p-6">
+          <h3 className="text-lg font-semibold leading-none tracking-tight">Delete Account</h3>
+          <p className="text-sm text-muted-foreground">Permanently remove your Personal Account and all of its contents from the Spacey platform. This action is not reversible, so please continue with caution.</p>
+        </div>
+        <div className="flex items-center p-6 pt-0">
+          <button className="inline-flex text-white items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background bg-black text-primary-foreground h-10 py-2 px-4" onClick={() => setShowConfirmDelete(true)}>
+            Delete now
+          </button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Danger;
---
+++ components/Navbar.js
@@ -0,0 +1,121 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { auth } from '../utils/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { AiFillPlayCircle, AiFillPauseCircle } from 'react-icons/ai';
+import { Hamburger } from './Hamburger';
+
+const audioFiles = ['/music/intro.wav', '/music/Dreamers.wav', '/music/WakaWaka.wav', '/music/Ozuna.wav','/music/OnTopOfTheWorld.wav', '/music/HayyaHayya.wav', '/music/Ramenez.wav', '/music/TheNights.wav', '/music/WeAreOneOleOla.wav', '/music/LaLaLa.wav','/music/WavingFlag.wav'];
+
+const Navbar = () => {
+  const [user, loading] = useAuthState(auth);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [audioIndex, setAudioIndex] = useState(0);
+  const [audio, setAudio] = useState(null);
+
+  useEffect(() => {
+    setAudio(new Audio(audioFiles[audioIndex]));
+  }, [audioIndex]);
+
+  return (
+    <nav className="fixed bg-white py-0 md:py-2 px-0 md:px-10 lg:px-20 w-full mx-auto z-50 border-b-2 border-gray-200 font-semibold">
+    <div className="hidden items-center md:visible md:flex md:justify-between px-10 py-2">
+    <div className="hover:cursor">
+      <Link href="/" legacyBehavior>
+          <a className="text-black font-mono transition-colors duration-200 hover:text-slate-500">Spacey</a>
+      </Link>
+    </div>
+
+    <div className="flex gap-4 justify-center items-center">
+            <div className="text-sm">
+              <Link href="/legends" legacyBehavior>
+                  <a className='text-black transition-colors duration-200 hover:text-slate-500'>Players</a>
+              </Link>
+            </div>
+            {user && (
+            <div className="text-sm">
+              <Link href="/chat" legacyBehavior>
+                  <a className='text-black transition-colors duration-200 hover:text-slate-500'>Chat</a>
+              </Link>
+            </div>
+            )}
+            {user && (
+            <div className="text-sm">
+              <Link href="/ranking" legacyBehavior>
+                <a className="text-black transition-colors duration-200 hover:text-slate-500">Ranking</a>
+              </Link>
+            </div>
+            )}
+            <div className="flex items-center gap-10">
+            {!user && (
+              <div className="flex items-center gap-6">
+                <button
+                  className="text-black text-sm transition-colors duration-200 hover:text-slate-500 flex items-center gap-1"
+                  onClick={() => setIsPlaying(!isPlaying)}
+                >
+                    <span className="text-sm">
+                        {isPlaying ? <p>Pause</p> : <p>Play</p>}
+                    </span>
+                    
+                    <span className="text-sm">
+                        {isPlaying ? <AiFillPauseCircle /> : <AiFillPlayCircle />}
+                    </span>
+                </button>              
+                <Link href="/auth/Login" legacyBehavior>
+                  <a className="text-black text-sm transition-colors duration-200 hover:text-slate-500">Join Now</a>
+                </Link>
+              </div>
+            )}
+            {user && (
+              <div className="flex items-center gap-6">
+                <button
+                  className="text-black text-sm transition-colors duration-200 hover:text-slate-500 flex items-center gap-1"
+                  onClick={() => setIsPlaying(!isPlaying)}
+                >
+                    <span className="text-sm">
+                        {isPlaying ? <AiFillPauseCircle /> : <AiFillPlayCircle />}
+                    </span>
+
+                    <span className="text-sm">
+                        {isPlaying ? <p>Pause</p> : <p>Play</p>}
+                    </span>
+                </button>
+                <Link href="/dashboard">
+                  <img className="w-8 h-8 rounded-full cursor-pointer" src={user.photoURL} />
+                </Link>
+              </div>
+            )}
+          </div>
+        </div> 
+
+    </div>
+
+    <div className="flex justify-between md:mt-3 md:hidden w-full max-w-3xl mx-auto px-5 md:px-10 py-5 items-center hover:cursor">
+        <ul className="flex items-center">
+            <Link legacyBehavior href="/">
+              <a className="text-black font-mono transition-colors duration-200 hover:text-slate-500">Spacey</a>    
+            </Link>
+        </ul>             
+        <Hamburger />
+    </div>
+    {isPlaying && (
+        <div className="fixed bottom-0 left-0 right-0 p-2 bg-white flex justify-center">
+          <p className="text-sm font-semibold text-center">Song: {audioFiles[audioIndex].replace(/^\/music\/|\.wav$/g, '')}</p>
+          <audio
+            autoPlay
+            src={audioFiles[audioIndex]}
+            onEnded={() => {
+              if (audioIndex === audioFiles.length - 1) {
+                setAudioIndex(0);
+              } else {
+                setAudioIndex(audioIndex + 1);
+              }
+            }}
+          />         
+        </div>
+      )}
+    </nav>
+  );  
+};
+
+export default Navbar;
