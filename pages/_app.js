4367c9947a0bd811daa424092af5d24257d27271
Dun Yan <ong.dy2004@gmail.com>
2024-05-23 17:49:09 +0800

    added calendar

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,21 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <div className='flex flex-col min-h-screen bg-white text-black'>
-      <div className='flex justify-center'>
-        <Navbar />
+    <ChakraProvider>
+      <div className='flex flex-col min-h-screen bg-white text-black'>
+        <div className='flex justify-center'>
+          <Navbar />
+        </div>
+        <main className='flex-grow py-10 md:pt-20 lg:pt-20 px-10'>
+          <Component {...pageProps} />
+        </main>
       </div>
-      <main className='flex-grow py-10 md:pt-20 lg:pt-20 px-10'>
-        <Component {...pageProps} />
-      </main>
-      <footer className='mt-auto'>
-        <Footer />
-      </footer>
-    </div>
+    </ChakraProvider>
   );
 }
 
